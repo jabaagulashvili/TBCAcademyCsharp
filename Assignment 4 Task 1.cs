2e69d0d083ffe09d451c6b4c95ac798985fb619e
jabaagulashvili <135153512+jabaagulashvili@users.noreply.github.com>
2023-06-12 18:00:31 +0400

    Update Assignment 4 Task 1.cs

--- Assignment 4 Task 1.cs
+++ Assignment 4 Task 1.cs
@@ -14,21 +14,21 @@ namespace TbcAcademy
 
         int[] numbers = new int[size];
 
-        // Input array elements
+        
         for (int x = 0; x < size; x++)
         {
             Console.Write($"Enter element {x + 1}: ");
             numbers[x] = int.Parse(Console.ReadLine());
         }
 
-        // Sort the array using the provided algorithm
+        
         for (int x = 0; x < size - 1; x++)
         {
             for (int y = 0; y < size - x - 1; y++)
             {
                 if (numbers[y] > numbers[y + 1])
                 {
-                    // Swap elements if they are out of order
+                    
                     int temp = numbers[y];
                     numbers[y] = numbers[y + 1];
                     numbers[y + 1] = temp;
@@ -36,7 +36,7 @@ namespace TbcAcademy
             }
         }
 
-        // Print the sorted array
+        
         Console.WriteLine("Sorted array:");
         foreach (int number in numbers)
         {
